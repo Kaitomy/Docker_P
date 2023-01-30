470c70a60be0f818b010b063b577e1a98f9cdada
ObraztsovaKsenia <virges06@gmail.com>
2023-01-30 17:50:40 +0300

    Практика первая

---
+++ lib/dart_application_3.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+import 'package:dart_application_3/controllers/app_token_controller.dart';
+import 'package:dart_application_3/controllers/app_user_controller.dart';
+
+import 'model/categories.dart';
+import 'model/finance.dart';
+import 'model/journal.dart';
+import 'model/user.dart';
+import 'package:conduit/conduit.dart';
+import 'package:dart_application_3/controllers/app_auth_controller.dart';
+
+class AppService extends ApplicationChannel {
+  late final ManagedContext managedContext;
+
+  @override
+  Future prepare() {
+    final persistentStore = _initDatebase();
+    managedContext = ManagedContext(
+        ManagedDataModel.fromCurrentMirrorSystem(), persistentStore);
+    return super.prepare();
+  }
+
+  @override
+  Controller get entryPoint => Router()
+    ..route('token/[:refresh]').link(
+      () => AppAuthController(managedContext),
+    )
+    ..route('user')
+    .link(AppTokenController.new)!
+    .link(() => AppUserController(managedContext))
+    ;
+//тут может быть ошибка
+  PersistentStore _initDatebase() {
+    final username = Platform.environment['DB_USERNAME'] ?? 'postgres';
+    final password = Platform.environment['DB_PASSWORD'] ?? '1';
+    final host = Platform.environment['DB_HOST'] ?? '127.0.0.1';
+    final port = int.parse(Platform.environment['D_PORT'] ?? '5432');
+    final databaseName = Platform.environment['DB_NAME'] ?? 'postgres';
+    return PostgreSQLPersistentStore(
+        username, password, host, port, databaseName);
+  }
+}
